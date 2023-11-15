dad51dbc9a7d8179ebb8c3f01f20e6da013b8af5
Samir-Sattarov <samirsattarov2004@gmail.com>
2023-11-15 18:56:50 +0500

    Task entity register adapter

---
+++ lib/core/entity/task_entity.g.dart
@@ -0,0 +1,97 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task_entity.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TaskEntityAdapter extends TypeAdapter<TaskEntity> {
+  @override
+  final int typeId = 1;
+
+  @override
+  TaskEntity read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TaskEntity(
+      id: fields[1] as String,
+      title: fields[2] as String,
+      description: fields[3] as String,
+      date: fields[4] as DateTime,
+      status: fields[5] as TaskStatus,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, TaskEntity obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(1)
+      ..write(obj.id)
+      ..writeByte(2)
+      ..write(obj.title)
+      ..writeByte(3)
+      ..write(obj.description)
+      ..writeByte(4)
+      ..write(obj.date)
+      ..writeByte(5)
+      ..write(obj.status);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskEntityAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class TaskStatusAdapter extends TypeAdapter<TaskStatus> {
+  @override
+  final int typeId = 2;
+
+  @override
+  TaskStatus read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 1:
+        return TaskStatus.todo;
+      case 2:
+        return TaskStatus.process;
+      case 3:
+        return TaskStatus.done;
+      default:
+        return TaskStatus.todo;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, TaskStatus obj) {
+    switch (obj) {
+      case TaskStatus.todo:
+        writer.writeByte(1);
+        break;
+      case TaskStatus.process:
+        writer.writeByte(2);
+        break;
+      case TaskStatus.done:
+        writer.writeByte(3);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskStatusAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
